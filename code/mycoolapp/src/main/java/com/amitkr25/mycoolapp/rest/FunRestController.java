3b4710908a07babbe0caaf5b5ba1d9a00d3efabb
zed1025 <amit251098@yahoo.in>
2024-10-21 22:59:21 +0530

    inject custom app properties

--- code/mycoolapp/src/main/java/com/amitkr25/mycoolapp/rest/FunRestController.java
+++ code/mycoolapp/src/main/java/com/amitkr25/mycoolapp/rest/FunRestController.java
@@ -1,10 +1,23 @@
 package com.amitkr25.mycoolapp.rest;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class FunRestController {
+    @Value("${coach.name}")
+    private String coachName;
+
+    @Value("${team.name}")
+    private String teamName;
+
+    // expose "/teaminfo"
+    @GetMapping("/teaminfo")
+    public String getTeamInfo() {
+        return ("Team " + teamName + "'s coach is " + coachName);
+    }
+
     // expose "/" endpoint to return "Hello World"
     @GetMapping("/")
     public String sayHello() {
